7ae297c1f4c31feab3a7cc849ebcdbb29fb6ffad
Mpho <mphomasemola103@gmail.com>
2025-11-02 20:02:23 +0200

    added a function

--- java.js
+++ java.js
@@ -1,1 +1,15 @@
-alert("Hello world!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "No man exist alone",
+    autoStart: true,
+    dealay: 1,
+    cursor: "",
+  });
+
+  poemElement.innerHTML = "No man exist alone";
+}
+
+let poemFormElement = document.querySelector("#ai-poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
