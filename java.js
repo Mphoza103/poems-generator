c0b6b66a6ccfdf0fad1e6b503082e4d6e48a3eee
Mpho <mphomasemola103@gmail.com>
2025-11-03 11:44:06 +0200

    added API key and Axios

--- java.js
+++ java.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated ");
   new Typewriter("#poem", {
-    strings: "No man exist alone",
+    strings: response.data.answer,
     autoStart: true,
     dealay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let rulesInput = document.querySelector("#user-rules");
+  let apiKey = "8a41t5bf055f3b901f1e9o1741935a65";
+  let prompt = `User instructions are: Generate a poem about ${rulesInput.value}`;
+  let context =
+    "You are a romantic poem expect and love to write short poems. Your mission is to write a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.Do not include a title to the poem. Sign the poem with 'Shecodes AI' inside a <strong> element at the end of the poem";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 
   poemElement.innerHTML = "No man exist alone";
 }
