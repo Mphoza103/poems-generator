8a28575c42868941f0eddc307e409ab83d1bf39b
Mpho <mphomasemola103@gmail.com>
2025-11-04 12:07:46 +0200

    worked on the UX.

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated ");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,16 +14,14 @@ function generatePoem(event) {
   let apiKey = "8a41t5bf055f3b901f1e9o1741935a65";
   let prompt = `User instructions are: Generate a poem about ${rulesInput.value}`;
   let context =
-    "You are a romantic poem expect and love to write short poems. Your mission is to write a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.Do not include a title to the poem. Sign the poem with 'Shecodes AI' inside a <strong> element at the end of the poem";
+    "You are an excellent poem writer and you are required to write a poem that follows user instructions. Write the 5 lines on any topic the user enters. Include a line <br /> and a signature at the bottom of the page 'Shecodes AI`.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a poem about ${rulesInput.value}`;
 
   axios.get(apiURL).then(displayPoem);
-
-  poemElement.innerHTML = "No man exist alone";
 }
 
 let poemFormElement = document.querySelector("#ai-poem-generator");
